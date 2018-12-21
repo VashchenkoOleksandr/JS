d4fc11aed2324ff5db6c1b8a939da51a00d9f230
GeekHubUser <icqvkontakt@gmail.com>
2018-12-21 21:45:14 +0200

    THE LAST of ...

--- HW8_JS3/All_level.js
+++ HW8_JS3/All_level.js
@@ -1,4 +1,6 @@
+//http://zhesha.github.io
 //Так і не зміг вирішити питання як читати лабіринт з консолі
+restart ();
 let level_map;
 let start_i;
 let start_j;
@@ -144,58 +146,113 @@ let arr_heigth = level_map [0].length;
             }
         }
     };
+
+//Переводимо лабіринт в цифрове значення https://habr.com/post/264189/ -1-стіна 0-можна ходити 1-фініш   
+    let map_numbers = [];
     
-};
-
-
-
-
-
-//Далі буду намагатись використовувати або хвильовий алгоритм або правило однієї руки
-
-
-let status;
-while (isFree ('north')) {
-    north ();
-};
-
-if ("next") {
-        status = 1;
+    for (let i = 0; i < arr_width; i++) {
+        map_numbers.push([]);
+        for (let j = 0; j < arr_heigth; j++) {
+            map_numbers [i].push(-1);
+            if (level_map [i][j] == ' ') {
+                map_numbers [i][j] = 0;
+            }
+            if (level_map [i][j] == '*') {
+                    map_numbers [i][j] = 1;
+            }
+        }
     };
 
-while (status != 1) {
-    east ();
-    if ("next") {
-        status = 1;
-    } else if (isFree ('south')) {
-        south ();
-        west ();
+    let exit = [[end_position_i, end_position_j]];
+    let way;
+    while (exit.length != 0); {
+        way = exit.shift();
+        if (way[0] + 1 < n && map_numbers[way[0] + 1][way[1]] == 0) {
+            map_numbers[way[0] + 1][way[1]] = map_numbers[way[0]][way[1]] + 1;
+            exit.push([way[0] + 1, way[1]]);
+        }
+        if (way[0] - 1 >= 0 && map_numbers[way[0] - 1][way[1]] == 0) {
+            map_numbers[way[0] - 1][way[1]] = map_numbers[way[0]][way[1]] + 1;
+            exit.push([way[0] - 1, way[1]]);
+        }
+        if (way[1] + 1 < m && map_numbers[way[0]][way[1] + 1] == 0) {
+            map_numbers[way[0]][way[1] + 1] = map_numbers[way[0]][way[1]] + 1;
+            exit.push([way[0], way[1] + 1]);
+        }
+        if (way[1] - 1 >= 0 && map_numbers[way[0]][way[1] - 1] == 0) {
+            map_numbers[way[0]][way[1] - 1] = map_numbers[way[0]][way[1]] + 1;
+            exit.push([way[0], way[1] - 1]);
+        }
+    }
+    
+//так зручніше читати код ніж писати start_position_i start_position_j
+    let i = start_i;
+    let j = start_j;
+    
+    while (!(start_position_i == end_position_i && start_position_j == end_position_j)) {
+        (i + 1 < arr_width && map_numbers [i + 1][j] == map_numbers [i][j] - 1) ?
+            (south (), i++)
+        : (i - 1 >= 0 && map_numbers [i - 1][j] == map_numbers [i][j] - 1) ?
+            (north (), i--)
+        : (j + 1 < arr_heigth && map_numbers [i][j + 1] == map_numbers [i][j] - 1) ?
+            (east (), j++)
+        : (west (), j--)
+    }
+}
+
+//НЕ ПРАЦЮЄ! КОД ЗАХОДИТЬ В ЦИКЛ І НЕ ВИХОДИТЬ З НЬОГО В КОНСОЛІ НІЧОГО НЕ ПИШЕ АЛЕ ПРОЦ НАВАНТАЖУЄ!
+
+
+let start = 1;
+switch (start) {
+    case 1:
+    if (isFree ('north')) {
         north ();
-        if (isFree ('south')) {
-            south ();
-            west ();
-        };
-    };
-};
-
-
-
-while (isFree('east') || isFree('west') || isFree('south') || isFree('north')) {
-if (isFree ('north')) {
-    north ();
-} else if (isFree ('east')) {
-    east ();
-} else if (isFree ('south')) {
-    south ();
-} else if (isFree ('west')) {
-    west ();
+        if (north () == 'end') break;
+        start = 3;
+    } else {
+     start = 4;
+    }
+    break;
+        
+    case 2:
+    if (isFree ('south')) {
+        south ();
+        if (south () == 'end') break;
+        start = 4;
+    } else {
+        start = 3;
+    }
+    break;
+        
+    case 3:
+    if (isFree ('west')) {
+        west ();
+        if (west () == 'end') break;
+        start = 2;
+    } else {
+        start = 1;
+    }
+    break;
+    
+    case 4:
+    if (isFree ('east')) {
+        east ();
+        if (east () == 'end') break;
+        start = 1;
+    } else {
+        start = 2;
+    }
+    break;
+    
+    default:
+        break;
 }
-}
-
-
-
-
-while () {
+    
+    
+    
+    
+if (north() != 'end'|| east() != 'end') {
     if (isFree ('north')) {
         while (isFree ('north')) {
             north ();	
@@ -213,50 +270,4 @@ while () {
             east ();	
         }
     }
-}
-
-
-
-
-
-//Розібратися з кодом
-this.checkPoints= function(p)
-{	
-  var count = p.length;
-  points = new Array();
-  //если закончен расчёт, тикаем
-  if(count==0 || bEnd) return;
-    //обходим точки
-    for(i=0;i<count;++i
-    {
-      //если достигли конца, то тикаем
-      if(p[i].i == endPoint.i && p[i].j == endPoint.j)
-      {
-	bEnd = true;
-	return;
-      }
-      //var x = 0;
-      //var y = 0;
-      //проверяем окружные 8 клеток
-      for( y=-1;y<=1;++y)
-	for( x=-1;x<=1;++x)
-	  if(!(x==0&&y==0))
-	    //проверка на выход за пределы поля
-	    if(checkPointLimit(p[i].i+y,p[i].j+x))
-	      if(mas[p[i].i+y][p[i].j+x] ==0)
-		//проверка на препятствия
-	        if(checkPointObstacle(p[i].i+y, p[i].j+x,p[i].i, p[i].j))
-		  //проверка значения
-		  if(checkPointValue (p[i].i+y,p[i].j+x,  mas[p[i].i][p[i].j]+((Math.abs(x)==Math.abs(y))?1.6:1), points))
-                    //если надо, рисуем волны
-		    if(showPriority && typeOfDrawing == 1) 
-		    {
-			var ar = new Array();
-			ar[0]={i:p[i].i+y, j:p[i].j+x};
-			delay+=10;
-			timer = setTimeout( function(ar ) {return function(){self.drawPointsAsynchron(ar)}}(ar), delay);
-		    }
-    }
-    //повторяем для новых клеток
-    this.checkPoints(points);
 }
