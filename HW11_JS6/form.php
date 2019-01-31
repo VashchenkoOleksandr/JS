98baf9291967b168dcc0297a8f3560fd4bf025c0
GeekHubUser <icqvkontakt@gmail.com>
2019-01-31 20:48:31 +0200

    Fix send data to server. Use eslint and clean code

--- HW11_JS6/form.php
+++ HW11_JS6/form.php
@@ -1,10 +1,10 @@
 <?php
-    
-$name = trim ($_POST ['name']);
-
+$requestBody = file_get_contents('php://input');
+$data = json_decode($requestBody, true);
+$name = trim($data['name']);
 if ($name == '') {
     echo 'You must input name!';
-}else{
-    file_put_contents ('hw11js6.txt', "$name \n", FILE_APPEND);
+} else {
+    file_put_contents('hw11js6.txt', "$name \n", FILE_APPEND);
     echo '1';
 }
--- HW11_JS6/get.js
+++ HW11_JS6/get.js
@@ -1,115 +1,75 @@
 window.onload = function () {
-
-    let inp_name = document.querySelector('input[name=name]');
-    
-    
+    const inpName = document.querySelector('input[name=name]');
     document.querySelector('#js_getButton').onclick = function () {
-        requestGet('http://localhost:5984/test/hw11', function (stringInHTML) {
+        requestGet('http://localhost:5984/test/hw11', (stringInHTML) => {
             document.querySelector('#js_get4Log').innerHTML = stringInHTML;
         });
-    }
-    
+    };
     document.querySelector('#js_putButton').onclick = function () {
-        requestPut ('http://localhost:5984/test/put');
-    }
-
+        requestPut('http://localhost:5984/test/put');
+    };
     document.querySelector('#postSend').onclick = function () {
-        let postText = 'You name is:'+ inp_name.value;
-        requestPost (postText);
-    }
-     
-}
- 
-//Розбирався з запитами на сервер і чому readyState === 4 
+        const postText = `You name is: ${inpName.value}`;
+        requestPost(postText);
+    };
+};
+
 function requestGet(url, callback) {
-    let callbackFunction = callback || function (stringInHTML) {};
-    let request = new XMLHttpRequest();
+    const callbackFunction = callback || function (stringInHTML) {};
+    const request = new XMLHttpRequest();
     let stringInHTML;
     request.onreadystatechange = function () {
-        if (request.readyState === 1 && request.status === 0) {
-            console.log('Server sleep request not initialized')
-            console.log('Ready State =', request.readyState);
-            console.log('Server status =',request.status);
-            stringInHTML = "Server status = " + request.status + ". Ready state = " + request.readyState + ". request.response Text = "+request.responseText;
-            document.querySelector('#js_get1Log').innerHTML = stringInHTML;
-        };
-        if (request.readyState === 2 && request.status === 200) {
-            console.log('Server recived request')
-            console.log('Ready State =', request.readyState);
-            console.log('Server status =',request.status);
-            stringInHTML = "Server status = " + request.status + ". Ready state = " + request.readyState + ". request.response Text = "+request.responseText;
-            document.querySelector('#js_get2Log').innerHTML = stringInHTML;
-        };
-        if (request.readyState === 3 && request.status === 200) {
-            console.log('Server start processing request')
-            console.log('Ready State =', request.readyState);
-            console.log('Server status =',request.status);
-            stringInHTML = "Server status = " + request.status + ". Ready state = " + request.readyState + ". request.response Text = "+request.responseText;
-            document.querySelector('#js_get3Log').innerHTML = stringInHTML;
-        };
         if (request.readyState === 4 && request.status === 200) {
-            console.log('Server ready and have "Ok" status')
-            console.log('Ready State =', request.readyState);
-            console.log('Server status =',request.status);
-            
-            stringInHTML = "Server status = " + request.status + ". Ready state = " + request.readyState + ". request.response Text = "+request.responseText;
-            callbackFunction (stringInHTML);
-        };
-    };
-    
-    request.open('GET', url);
-    request.send();
-};
-
-
-// PUT запит
-function requestPut (putURL) {
-
-    let putData = {};
-        putData.firstname = 'Sasha';
-        putData.lastname  = 'Vashchenko';
-    let putReq = JSON.stringify(putData);
-
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-    if (request.readyState === 4 && request.status === 201) {
-            console.log('Server finish PUT processing')
-            console.log('Ready State =', request.readyState);
-            console.log('Server status =',request.status);
-            let stringInHTML = 'Server status = ' + request.status + '. Ready state = ' + request.readyState + '. request.response Text = '+request.responseText+'. Input text '+putReq+'. File URL = '+ putURL;
-            document.querySelector('#js_putLog').innerHTML = stringInHTML;
-        } else if (request.status === 409) {
-            console.log('Ready State =', request.readyState);
-            console.log('Server status =',request.status);
-            document.querySelector('#js_putLog').innerHTML = 'We have a some problem. ERROR 409 (Document update conflict). Delete the put document or change the put request name';
-        }; 
-    };
-    
-    request.open("PUT", putURL, true);
-    request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    request.send(putReq);
-};
-
-
-
-
-function requestPost (postText) {
-    var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
+            console.log('Server ready and have "Ok" status');
             console.log('Ready State =', request.readyState);
             console.log('Server status =', request.status);
-        if (request.readyState === 4 && request.status === 200) {
-                document.querySelector('#js_postLog').innerHTML = 'FINALLY POST REQUEST IS WORKING. Thank you Serhii ' + request.responseText;  
-//            if (request.responseText == '1') {
-//                document.querySelector('form').style.display = 'none';    
-//                document.querySelector('#js_postLog').innerHTML = 'FINALLY POST REQUEST IS WORKING.'  
-//            } else {
-//                document.querySelector('#js_postLog').innerHTML = 'We have a problem. '  
-//            };
-        };
+            stringInHTML = `Server status = ${request.status}. Ready state = ${request.readyState}. request.response Text = ${request.responseText}`;
+            callbackFunction(stringInHTML);
+        }
     };
-    request.open("POST", 'form.php');
+    request.open('GET', url);
+    request.send();
+}
+
+function requestPut(putURL) {
+    const putData = {};
+    putData.firstname = 'Sasha';
+    putData.lastname = 'Vashchenko';
+    const putReq = JSON.stringify(putData);
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 201) {
+            console.log('Server finish PUT processing');
+            console.log('Ready State =', request.readyState);
+            console.log('Server status =', request.status);
+            const stringInHTML = `Server status = ${request.status}. Ready state = ${request.readyState}. request.response Text = ${request.responseText}. Input text ${putReq}. File URL = ${putURL}`;
+            document.querySelector('#js_putLog').innerHTML = stringInHTML;
+        } else if (request.status === 409) {
+            console.log('Ready State PUT =', request.readyState);
+            console.log('Server status =', request.status);
+            document.querySelector('#js_putLog').innerHTML = 'We have a some problem. ERROR 409 (Document update conflict). Delete the put document or change the put request name';
+        }
+    };
+    request.open('PUT', putURL, true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    request.send(putReq);
+}
+
+function requestPost(postText) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        console.log('Ready State =', request.readyState);
+        console.log('Server status =', request.status);
+        if (request.readyState === 4 && request.status === 200) {
+            if (request.responseText === '1') {
+                document.querySelector('form').style.display = 'none';
+                document.querySelector('#js_postLog').innerHTML = 'POST REQUEST IS WORKING. Response text get';
+            } else {
+                document.querySelector('#js_postLog').innerHTML = 'We have a problem. ';
+            }
+        }
+    };
+    request.open('POST', 'form.php');
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     request.send(postText);
-};
+}
