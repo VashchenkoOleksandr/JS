752126bd1776e6880ffa63cbda45378690328e41
GeekHubUser <icqvkontakt@gmail.com>
2019-02-24 21:51:24 +0200

    Way of hero

--- HeroesTour/angular-tour-of-heroes/src/app/app.component.ts
+++ HeroesTour/angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  title = 'My first step in the angular tour of heroes';
 }
