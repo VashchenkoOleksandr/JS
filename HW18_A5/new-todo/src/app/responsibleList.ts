eb329bbc536ab2b3b6b881952333b9c1d0571ff5
GeekHubUser <icqvkontakt@gmail.com>
2019-03-18 23:27:35 +0200

    Fight from the angular. Pauses

---
+++ HW18_A5/new-todo/src/app/responsibleList.ts
@@ -0,0 +1,4 @@
+export class ResponsibleList {
+
+  constructor(public responsible: string) { }
+}
--- HW18_A5/new-todo/src/app/todo/todo.component.ts
+++ HW18_A5/new-todo/src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../todoItem';
+import {ResponsibleList} from '../responsibleList';
 
 
 @Component({
@@ -8,15 +9,26 @@ import { TodoItem } from '../todoItem';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+
+  constructor() {}
+
+  responsible: ResponsibleList[] = [
+    {responsiblePerson: 'Vashchenko'},
+    {responsiblePerson: 'Test1'},
+    {responsiblePerson: 'Test2'}
+  ];
+
+  addTestRespons(respons: string) {
+    this.responsible.push(new ResponsibleList(respons));
+  }
+  
   title = 'New ToDO';
 
   items: TodoItem[] = [
-    {id: 'testId1', title: 'Test title1', responsible: 'Me', dueDate: 'Test date', status: 'new'},
-    {id: 'testId2', title: 'Test title2', responsible: 'Me', dueDate: 'Test date2', status: 'in process'},
-    {id: 'testId3', title: 'Test title3', responsible: 'Me', dueDate: 'Test date3', status: 'done'}
+    {id: 'testId1', title: 'Test title1', responsible: 'Vashchenko', dueDate: 'Test date', status: 'new'},
+    {id: 'testId2', title: 'Test title2', responsible: 'Vashchenko', dueDate: 'Test date2', status: 'in process'},
+    {id: 'testId3', title: 'Test title3', responsible: 'Vashchenko', dueDate: 'Test date3', status: 'done'}
   ];
-
-  constructor() {}
 
   ngOnInit(): void {}
 
