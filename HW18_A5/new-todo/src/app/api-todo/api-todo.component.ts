efc86ec1ba963692affd6ca9511842837c21120c
GeekHubUser <icqvkontakt@gmail.com>
2019-03-25 23:14:45 +0200

    Create the status filter

--- HW18_A5/new-todo/src/app/api-todo/api-todo.component.ts
+++ HW18_A5/new-todo/src/app/api-todo/api-todo.component.ts
@@ -8,18 +8,38 @@ import { TodoItem } from '../todoItem';
   <p>It is works</p>
   <li *ngFor="let todo of todoItem">
     ID: {{todo?.id}} Title: {{todo?.title}} Title: {{todo?.responsible}} Title: {{todo?.dueDate}} Title: {{todo?.status}}
-  </li>`,
+  </li>
+  <button (click)="takeAllData()">Take all task list</button>
+  <button (click)="takeInProcess()">Take in process</button>
+  <button (click)="takeDone()">Take done task</button>
+  <li *ngFor="let todo of todoItems">
+    ID: {{todo?.id}} Title: {{todo?.title}} responsible: {{todo?.responsible}} dueDate: {{todo?.dueDate}} status: {{todo?.status}}
+  </li>
+  `,
   providers: [HttpService],
   styleUrls: ['./api-todo.component.scss']
 })
 export class ApiTodoComponent implements OnInit {
 
   todoItem: TodoItem[] = [];
+  todoItems: TodoItem[] = [];
 
   constructor(private httpService: HttpService) { }
 
   ngOnInit() {
     this.httpService.getNewData().subscribe(data => this.todoItem = data);
+  }
+
+  takeAllData() {
+    this.httpService.getData().subscribe(data => this.todoItems = data);
+  }
+
+  takeInProcess() {
+    this.httpService.getInProcess().subscribe(data => this.todoItems = data);
+  }
+
+  takeDone() {
+    this.httpService.getDone().subscribe(data => this.todoItems = data);
   }
 
 }
--- HW18_A5/new-todo/src/app/http.service.ts
+++ HW18_A5/new-todo/src/app/http.service.ts
@@ -20,4 +20,34 @@ export class HttpService {
     }));
   }
 
+  getData(): Observable<TodoItem[]> {
+    return this.http.get('http://localhost:4200/assets/task.json').pipe(map(data => {
+      const taskList = data["taskList"];
+      return taskList.map(function(todo: any) {
+          return {id: todo.id, title: todo.title, responsible: todo.responsible, dueDate: todo.dueDate, status: todo.status};
+      });
+    }));
+  }
+
+  getInProcess(): Observable<TodoItem[]> {
+    return this.http.get('http://localhost:4200/assets/task.json').pipe(map(data => {
+      const taskList = data["taskList"];
+      return taskList.filter(function (todo: any) {
+        if (todo.status === 'in process') {
+          return {id: todo.id, title: todo.title, responsible: todo.responsible, dueDate: todo.dueDate, status: todo.status};
+        }
+      });
+    }));
+  }
+  getDone(): Observable<TodoItem[]> {
+    return this.http.get('http://localhost:4200/assets/task.json').pipe(map(data => {
+      const taskList = data["taskList"];
+      return taskList.filter(function(todo: any) {
+        if (todo.status === 'done') {
+          return {id: todo.id, title: todo.title, responsible: todo.responsible, dueDate: todo.dueDate, status: todo.status};
+        }
+      });
+    }));
+  }
+
 }
